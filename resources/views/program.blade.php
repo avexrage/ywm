29ca5cbc5dc51ca42c832c4f98e4451bc94e0b5d
maulana <farisjeh425@gmail.com>
2024-03-22 14:06:27 +0700

    Membuat Route

---
+++ resources/views/program.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Pilih menu program</h1>
+    <h2>ini adalah program {{ $program1 }}</h2>
+    <br>
+    <h2>ini adalah program {{ $program2 }}</h2>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function(){
+    return 'ini halaman about';
+});
+
+// Route::get('/program', function () {
+//     return view('program');
+// });
+
+Route::view('/program', 'program', ['program1' => 'day care', 
+'program2' => 'grha wredha mulya']);
+
+Route::redirect('/here', '/there',);
+
+
+//memanggil user berdasarkan id/memanggil detail/route memanggil berdasarkan parameter
+Route::get('/user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+//file detail.blade.php di dalam folder product
+Route::get('/product/{id}', function ($id) {
+    return view('product.detail', ['id' => $id]);
+});
+
+//prefixs route
+Route::prefix('administrator')->group(function () {
+    Route::get('/profil-admin', function(){
+        return 'profil admin';
+    });
+    
+    Route::get('/contact-admin', function(){
+        return 'contact admin';
+    });
+});
