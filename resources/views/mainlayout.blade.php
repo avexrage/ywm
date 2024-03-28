c2fc68bbed3d0613afb94402e0abc44ec9d0ceb9
Faris-Laptop <poayof@gmail.com>
2024-03-28 21:40:15 +0700

    navbar -login?

--- resources/views/mainlayout.blade.php
+++ resources/views/mainlayout.blade.php
@@ -3,40 +3,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yayasan Wredha Mulya | Beranda</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Beranda | Yayasan Wredha Mulya</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .navbar-brand-vertical {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+        
+        .navbar-brand-vertical .brand-top,
+        .navbar-brand-vertical .brand-bottom {
+            line-height: 1;
+        }
 
+        .navbar-brand-vertical .brand-top,
+        .navbar-brand-vertical .brand-bottom {
+            font-size: 1rem;
+        }
+
+        .custom-margin {
+            margin-right: 50px; /* Jarak ke menu navigasi */
+        }
+
+        .navbar-nav {
+            display: flex; /* Menjadikan ul sebagai flex container */
+            align-items: center; /* Menyelaraskan item di tengah secara vertikal */
+            gap: 20px; /* Memberikan jarak antar item menu */
+        }
+    </style>
+</head>
+<body>
     <nav class="navbar navbar-expand-lg bg-body-light">
-  <div class="container-fluid">
-  <img src="{{ asset('ikon/ywm.jpg') }}" alt="Yayasan Wredha Mulya Logo" width="70" height="64">
-    <a class="navbar-brand text-success" href="#">Yayasan Wredha Mulya</a>
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active text-success" aria-current="page" href="#">Beranda</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active text-success" aria-current="page" href="#">Program</a>
-        </li> <li class="nav-item">
-          <a class="nav-link active text-success" aria-current="page" href="#">Fasilitas</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <div class="container-fluid">
+            <img src="{{ asset('ikon/ywm.jpg') }}" alt="Yayasan Wredha Mulya Logo" width="70" height="64" class="me-auto">
+            <a class="navbar-brand navbar-brand-vertical text-success custom-margin">
+                <span class="brand-top">Yayasan</span>
+                <span class="brand-bottom">Wredha Mulya</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active text-success" aria-current="page" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-success" href="#">Program</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-success" href="#">Fasilitas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-success" href="#">Kontak Kami</a>
+                    </li>
+                </ul> 
+            </div>
+        </div>
+    </nav>
 </body>
 </html>
